62f6faa9bb3807ea2e91284e3b436873a3be79a4
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-25 18:04:19 -0600

    Render a child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IdeasComponent } from './ideas/ideas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdeasComponent
   ],
   imports: [
     BrowserModule
