95a394fd38eca518b399304a16c0e4e6b0ce2696
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-25 18:31:15 -0600

    Create a method to return a new message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ideabox-practice';
+
+  showTitle() {
+    return 'another title'
+  }
+
 }
+
+// All of the logic for your main app would go here!
+// The logic is kept out of the html file, compared to React where the JSX is embedded into the component file
